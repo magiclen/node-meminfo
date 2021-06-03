a60054b5072bf1ac7c39c2639f72c06e02da7b4a
Magic Len <len@magiclen.org>
2021-06-03 15:15:24 +0800

    fix tests

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -5,15 +5,14 @@ const meminfo = require("..");
 
 describe("get", function () {
     it("should get the data from /proc/meminfo", function () {
-        var result = meminfo.get();
+        const result = meminfo.get();
         expect(result).to.be.an("object");
     });
 });
 
-
 describe("free", function () {
     it("should get the data from /proc/meminfo and the output format is like the command \"free\"", function () {
-        var result = meminfo.free();
+        const result = meminfo.free();
         expect(result).to.be.an("object");
         expect(result.mem).to.be.an("object");
         expect(result.swap).to.be.an("object");
